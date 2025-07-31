d819992f1136cb9b62279eef9147e77313f7b108
Viswanathan-19 <perumalvishwa70@gmail.com>
2025-07-31 09:34:02 +0530

    Feat:Added Summary Screen to Display the Results

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,30 +1,41 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/questions_summary.dart';
 
-class ResultsScreen extends StatelessWidget{
-const ResultsScreen({super.key,
-required this.chosenAnswers,
-});
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key, required this.chosenAnswers});
 
+  final List<String> chosenAnswers;
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summary;
+  }
 
-final List<String> chosenAnswers;
   @override
   Widget build(BuildContext context) {
-      return SizedBox(
-      width:double.infinity,
+    return SizedBox(
+      width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('hbn'),
-            SizedBox(height: 30,),
-            Text('List of answers and questions '),
-            SizedBox(height: 30,),
-            TextButton(onPressed:(){},
-             child: Text('Restart Quiz!'))
+            SizedBox(height: 30),
+            QuestionsSummary(getSummaryData()),
+            SizedBox(height: 30),
+            TextButton(onPressed: () {}, child: Text('Restart Quiz!')),
           ],
-         ),
+        ),
       ),
-      );
+    );
   }
 }
